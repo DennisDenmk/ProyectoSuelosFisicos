f64e8aaa2e2aebf270d480af7e05b819b2be442c
DennisDenmk <dm02102002@gmail.com>
2025-02-01 23:21:55 -0500

    cambios

--- resources/views/docente/RegistrarParcelas.blade.php
+++ resources/views/docente/RegistrarParcelas.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registrar Parcelas</title>
-    <link rel="stylesheet" href="../CSS/ResgitrarParcelas.css">
+    <link rel="stylesheet" href="{{asset('css/ResgitrarParcelas.css')}}">
 </head>
 
 <body>
