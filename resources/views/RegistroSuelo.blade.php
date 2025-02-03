713b4c407c328f4c8b7abbfdf5aed26c5338804a
DennisDenmk <dm02102002@gmail.com>
2025-02-02 22:51:18 -0500

    prueba css

--- resources/views/RegistroSuelo.blade.php
+++ resources/views/RegistroSuelo.blade.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro de Muestras</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-GTCkn69vnE5l6UhMhL6b+np6HkdhRRNF75yqe2HhmiXxXbtRmEbK4uL4aAoZ3Ugm" crossorigin="anonymous">
     <script src="{{ asset('js/validaciones.js') }}" defer></script>
 </head>
 
