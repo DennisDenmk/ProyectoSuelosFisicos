0947fb75e3c8544442c95a6d5287e3b26d8e0f7b
SKJurado <oskarjurado6@gmail.com>
2025-02-03 21:12:28 -0500

    Editar Parcelas y Muestra css

--- resources/views/docente/EditarParcela.blade.php
+++ resources/views/docente/EditarParcela.blade.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Editar Parcela</title>    
-    <link rel="stylesheet" href="{{asset('css/EditarParcela.css')}}">
+    <title>Editar Parcela</title>
+    <link rel="stylesheet" href="{{ asset('css/EditarParcela.css') }}">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 
@@ -29,45 +29,53 @@
         <script>
             let errors = @json($errors->all());
             Swal.fire({
-                icon: 'error',
-                title: 'Error al procesar',
-                html: errors.map(error => <li>${error}</li>).join(''),
-            });
+                        icon: 'error',
+                        title: 'Error al procesar',
+                        html: errors.map(error => < li > $ {
+                                error
+                            } < /li>).join(''),
+                        });
         </script>
     @endif
 
     <div class="container">
-        <form action="{{ route('parcelas.actualizar', ['id' => $parcela->parc_id]) }}" method="POST">
-            @csrf
-            @method('PUT')
 
-            <div class="mb-3">
-                <label for="parc_nombre" class="form-label">Nombre de la Parcela</label>
-                <input type="text" name="parc_nombre" class="form-control" value="{{ $parcela->parc_nombre }}">
-            </div>
+        <div class="right-column">
+            <form action="{{ route('parcelas.actualizar', ['id' => $parcela->parc_id]) }}" method="POST">
+                @csrf
+                @method('PUT')
 
-            <div class="mb-3">
-                <label for="parc_area" class="form-label">Área</label>
-                <input type="number" name="parc_area" class="form-control" value="{{ $parcela->parc_area }}" required>
-            </div>
+                <div class="form-group">
+                    <label for="parc_nombre" class="form-label">Nombre de la Parcela</label>
+                    <input type="text" name="parc_nombre" class="form-control" value="{{ $parcela->parc_nombre }}">
+                </div>
 
-            <div class="mb-3">
-                <label for="parc_coord_la" class="form-label">Coordenada Latitud</label>
-                <input type="number" name="parc_coord_la" class="form-control" step="0.01"  value="{{ $parcela->parc_coord_la }}" required>
-            </div>
+                <div class="form-group">
+                    <label for="parc_area" class="form-label">Área</label>
+                    <input type="number" name="parc_area" class="form-control" value="{{ $parcela->parc_area }}"
+                        required>
+                </div>
 
-            <div class="mb-3">
-                <label for="parc_coord_lo" class="form-label">Coordenada Longitud</label>
-                <input type="number" name="parc_coord_lo" class="form-control" step="0.01"  value="{{ $parcela->parc_coord_lo }}" required>
-            </div>
+                <div class="form-group">
+                    <label for="parc_coord_la" class="form-label">Coordenada Latitud</label>
+                    <input type="number" name="parc_coord_la" class="form-control" step="0.01"
+                        value="{{ $parcela->parc_coord_la }}" required>
+                </div>
 
-            <div class="mb-3">
-                <label for="parc_descripcion" class="form-label">Descripción</label>
-                <textarea name="parc_descripcion" class="form-control">{{ $parcela->parc_descripcion }}</textarea>
-            </div>
+                <div class="form-group">
+                    <label for="parc_coord_lo" class="form-label">Coordenada Longitud</label>
+                    <input type="number" name="parc_coord_lo" class="form-control" step="0.01"
+                        value="{{ $parcela->parc_coord_lo }}" required>
+                </div>
 
-            <button type="submit" class="btn btn-success">Guardar Cambios</button>
-        </form>
+                <div class="form-group">
+                    <label for="parc_descripcion" class="form-label">Descripción</label>
+                    <textarea name="parc_descripcion" class="form-control">{{ $parcela->parc_descripcion }}</textarea>
+                </div>
+
+                <button type="submit" class="btn btn-success">Guardar Cambios</button>
+            </form>
+        </div>
     </div>
 </body>
 
